8c5f340ee720657ca06ef3bf2cac92cae860ea28
Huzaifa Raghav <windowpane1712@gmail.com>
2022-07-09 23:04:13 +0800

    Try to subscribe to data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,43 @@ import "./App.css";
 import { UserContext } from "./services/userContext";
 import LoginPage from "./views/LoginPage";
 import { Title } from "@mantine/core";
+import { useQuery, gql, useSubscription } from '@apollo/client';
+import { User } from './types/user.type';
+import { useState } from 'react';
+
+const READ_CLIENTS = gql`
+  query ReadClients {
+    readClients {
+      username
+    }
+  } 
+`
+
+function Subscription() {
+  const TEST_SUBSCRIPTION = gql`
+    subscription ClientRead {
+    clientRead {
+      text
+    }
+  }
+  `;
+
+  //const [test, setTest] = useState([]);
+  const { data, loading } = useSubscription(TEST_SUBSCRIPTION, {
+    onSubscriptionData(options) {
+        const { subscriptionData } = options;
+        console.log("New data:", subscriptionData);
+    },
+  });
+
+  return (
+    <div>
+      <p>Data:{data}</p> 
+    </div>
+  )
+
+
+}
 
 function App() {
   const { user } = useContext(UserContext);
--- frontend/src/createClient.js
+++ frontend/src/createClient.js
@@ -1,11 +1,25 @@
 import { createHttpLink } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 import { ApolloClient, InMemoryCache, } from '@apollo/client';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
 
-const httpLink = createHttpLink({
-    uri: process.env.REACT_APP_SERVER_URI,
-  })
+
+// const httpLink = createHttpLink({
+//     uri: process.env.REACT_APP_SERVER_URI,
+//   })
+
+const httpLink = new HttpLink({
+    uri: process.env.REACT_APP_SERVER_URI
+})
   
+const wsLink = new GraphQLWsLink(createClient({
+    uri: 'ws://localhost:4001/graphql',
+    url:'ws://localhost:4001/graphql'
+}));
+
 const authLink = setContext((_, { headers }) => {
 return {
     headers: {
@@ -15,8 +29,25 @@ return {
 }
 })
 
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+  );
+
+  const client = new ApolloClient({
+    link: authLink.concat(splitLink),
     cache: new InMemoryCache(),
 });
 
